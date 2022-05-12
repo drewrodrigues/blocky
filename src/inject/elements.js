f7300eb046ada8450c6355dfead89e8083858481
Drew Rodrigues <thesimpledev@gmail.com>
2022-05-12 14:19:09 +0200

    Add title and subtitle to  section

--- src/inject/elements.js
+++ src/inject/elements.js
@@ -4,9 +4,32 @@ function Sidebar() {
   return sidebar;
 }
 
+function Section(title, subtitle) {
+  const sectionElement = document.createElement("section");
+  sectionElement.classList.add("section");
+
+  const titleElement = document.createElement("h2");
+  titleElement.textContent = title;
+  titleElement.classList = "section-title";
+  sectionElement.append(titleElement);
+
+  const subtitleElement = document.createElement("h3");
+  subtitleElement.textContent = subtitle;
+  subtitleElement.classList = "section-subtitle";
+  sectionElement.append(subtitleElement);
+
+  return sectionElement;
+}
+
 function Buttons(sortedBlocks) {
+  const sectionElement = Section(
+    "Generated Blocks",
+    "Click on a block then your calendar to create a block"
+  );
+
   const buttonContainer = document.createElement("header");
   buttonContainer.classList = "button-container";
+  sectionElement.append(buttonContainer);
 
   for (const block of sortedBlocks) {
     const title = Object.keys(block)[0];
@@ -36,5 +59,5 @@ function Buttons(sortedBlocks) {
     buttonContainer.append(button);
   }
 
-  return buttonContainer;
+  return sectionElement;
 }
