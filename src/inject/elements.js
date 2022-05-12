830d36aef7782174180952c1e22a327426b38db2
Drew Rodrigues <thesimpledev@gmail.com>
2022-05-12 13:59:42 +0200

    Split some logic into seperate files

---
+++ src/inject/elements.js
@@ -0,0 +1,37 @@
+function createGeneratedBlocksSection(sortedBlocks) {
+  const container = document.createElement("aside");
+  container.classList.add("container");
+  const buttonContainer = document.createElement("header");
+  buttonContainer.classList = "button-container";
+  container.append(buttonContainer);
+
+  for (const block of sortedBlocks) {
+    const title = Object.keys(block)[0];
+    const calendar = Object.values(block)[0].calendar;
+
+    const button = document.createElement("button");
+    button.classList += "block-button";
+    button.onclick = () => {
+      selectedButton = {
+        calendar,
+        title,
+      };
+      const alreadyActiveButton = document.querySelector(
+        ".block-button.active"
+      );
+      if (alreadyActiveButton) {
+        alreadyActiveButton.classList.remove("active");
+      }
+      button.classList.add("active");
+    };
+    button.style.backgroundColor = Object.values(block)[0].style;
+
+    const text = document.createElement("span");
+    text.textContent = title;
+    button.append(text);
+
+    buttonContainer.append(button);
+  }
+
+  return container;
+}
