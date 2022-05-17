d8500015ef41f77866fd23e98c1ef67097299bd5
Drew Rodrigues <thesimpledev@gmail.com>
2022-05-17 15:31:04 +0200

    Add live-server, tailwind & concurrently for website build

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/website/**/*.{html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ webpack.plugin.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/plugin/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          publicPath: '../src/plugin/icons',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+}
