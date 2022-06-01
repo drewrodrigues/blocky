9cbbcc63c563a440022eccd66da309f8eb333645
Drew Rodrigues <thesimpledev@gmail.com>
2022-06-01 13:49:19 +0200

    Just use BlockContainer

--- plugin/src/components/blocks/BlockContainer.tsx
+++ plugin/src/components/blocks/BlockContainer.tsx
@@ -5,10 +5,11 @@ import { BlockButton } from './BlockButton'
 interface Props {
   blocks: Block[]
   onClick?: (block: Block) => void
+  onRightClick?: (block: Block) => void
   selectedBlock?: Block
 }
 
-export function GeneratedBlocks(props: Props) {
+export function BlockContainer(props: Props) {
   return (
     <div className="overflow-y-scroll flex-[1_1_0] border border-grey-light p-[10px] rounded-[10px]">
       {props.blocks.map((block) => {
@@ -20,6 +21,7 @@ export function GeneratedBlocks(props: Props) {
             isHighlighted={isSelected}
             isLoading={isSelected}
             onClick={() => props.onClick?.(block)}
+            onRightClick={() => props.onRightClick?.(block)}
           />
         )
       })}
