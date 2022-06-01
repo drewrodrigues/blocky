9ea1e3abdcfd68e183200bba9325f2a627d45a14
Drew Rodrigues <thesimpledev@gmail.com>
2022-06-01 14:00:40 +0200

    Add cache utils

---
+++ plugin/src/utils/cache.ts
@@ -0,0 +1,28 @@
+import { BlockByTitle } from './types'
+
+type CacheFor = 'SavedBlocks' | 'GeneratedBlocks'
+
+export const getCacheKey = (keyEnding: CacheFor) =>
+  `BLOCKY_CACHE_BASE-${keyEnding}`
+
+export function cacheBlocks(cacheFor: CacheFor, blocks: BlockByTitle) {
+  const CACHE_KEY = getCacheKey(cacheFor)
+
+  // async write to not block main thread
+  setTimeout(() => {
+    localStorage.setItem(CACHE_KEY, JSON.stringify(blocks))
+  }, 0)
+}
+
+export function pullCachedBlocks(cacheFor: CacheFor) {
+  const CACHE_KEY = getCacheKey(cacheFor)
+
+  const unparsedJson = localStorage.getItem(CACHE_KEY)
+  try {
+    const parsedBlocks = JSON.parse(unparsedJson || '{}')
+    return parsedBlocks as BlockByTitle
+  } catch {
+    console.error(`pullCachedBlocks(): Failed to parse blocks for ${cacheFor}`)
+    return {}
+  }
+}
