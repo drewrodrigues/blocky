e682c748eaefb0a758fcffb1aacfd18286a6245d
Drew Rodrigues <thesimpledev@gmail.com>
2022-06-05 13:08:55 +0200

    Update jumbo area (mobile first)

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blocky: {
+          blue: {
+            lighter: "rgba(168,233,255, 0.25)",
+            light: "#A8E9FF",
+            medium: "#41D3FF",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
