f7f1cd25835fce0f2b19d0cf6d73f6bd2076c796
Drew Rodrigues <rodandrew95@gmail.com>
2023-06-22 15:16:00 -0700

    Add test outlines

---
+++ plugin/src/utils/__tests__/generatedBlockParser.test.ts
@@ -0,0 +1,7 @@
+// automock
+// mock out getElementOrThrow && getElementsOrThrow
+
+describe('listenToViewAndGenerateBlocks', () => {
+  // listens for blocks every 1000ms
+  // it returns its interval
+})
---
+++ plugin/src/utils/__tests__/modalListener.test.ts
@@ -0,0 +1,20 @@
+// automock
+// mock out getElementOrThrow && getElementsOrThrow
+
+describe('listenForModalOpen', () => {
+  // it checks for the modal every 100ms
+  // it returns its interval
+  //
+  // when modal found:
+  // - it inserts title
+  // - it clicks calendar button
+  // - it clicks calendar option
+  // - it clicks save button
+  // - it calls back isCreatingEvent with true
+  // - it calls back isCreatingEvent with false once done
+  //
+  // it throws an error when unable to insert title
+  // it throws an error when unable to click calendar button
+  // it throws an error when unable to click calendar option
+  // it throws an error when unable to click save button
+})
