f71d2a261f77e6a1e73aa2fa6a5720ea56ba80d6
Drew Rodrigues <rodandrew95@gmail.com>
2023-06-22 14:42:20 -0700

    Align placeholder header better

--- plugin/src/components/placeholder/EmptyPlaceholder.tsx
+++ plugin/src/components/placeholder/EmptyPlaceholder.tsx
@@ -11,14 +11,16 @@ interface Props {
 export function EmptyPlaceholder(props: Props) {
   return (
     <div
-      className="rounded-[10px] text-grey-medium h-[200px] flex items-center justify-center text-[14px] flex-col border border-grey-light p-[25px] text-center flex-grow pointer-events-none"
+      className="rounded-[10px] text-grey-medium h-[200px] flex items-start justify-center text-[14px] flex-col border border-grey-light p-[25px] flex-grow pointer-events-none"
       style={SharedStyles.containerBoxShadow}
     >
-      <FontAwesomeIcon
-        icon={faInfoCircle}
-        className="mb-[10px] text-[16px] text-[#42D3FC]"
-      />
-      <p className="mb-[5px]">{props.title}</p>
+      <header className="flex items-center mb-[10px]">
+        <FontAwesomeIcon
+          icon={faInfoCircle}
+          className="text-[16px] text-[#42D3FC] mr-[7.5px]"
+        />
+        <p className="font-bold text-[12px] leading-[1]">{props.title}</p>
+      </header>
       <p className="text-[12px] rounded-[5px]">{props.suggestion}</p>
     </div>
   )
