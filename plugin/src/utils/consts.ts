db81ea724876baf2f564b11d08107828a2b932fd
Drew Rodrigues <rodandrew95@gmail.com>
2023-06-23 10:28:45 -0500

    Fix calendar option selector

--- plugin/src/utils/consts.ts
+++ plugin/src/utils/consts.ts
@@ -3,7 +3,7 @@ export enum CALENDAR_SELECTOR {
   MODAL = '.K0f0Xc',
   MODAL_TITLE_INPUT = '[aria-label="Add title"]',
   CALENDAR_OPTION_BUTTON = '[data-key="calendar"]',
-  CALENDAR_OPTION = 'div[role="presentation"] div[role="option"]',
+  CALENDAR_OPTION = 'div[role="listbox"][aria-label="Calendar"] > div[role="presentation"] > div',
   SAVE_BUTTON = '[jsname="x8hlje"]',
   SIDEBAR_CONTAINER = '.tEhMVd',
 }
