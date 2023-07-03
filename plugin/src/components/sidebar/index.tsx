50ce79c5f33a89441efa17b10bec51175d6d6280
Drew Rodrigues <rodandrew95@gmail.com>
2023-07-03 15:37:04 -0500

    Increment minor

--- plugin/src/components/sidebar/index.tsx
+++ plugin/src/components/sidebar/index.tsx
@@ -10,7 +10,7 @@ import { SidebarSectionDocumentation } from './SidebarSectionDocumentation'
 import { SidebarToggle } from './sidebarToggle'
 import '../../assets/style.css' // TODO: do this better my guy
 
-export class _Sidebar extends React.Component { 
+export class _Sidebar extends React.Component {
   static Section = SidebarSection
   static Toggle = SidebarToggle
 }
@@ -34,92 +34,94 @@ export function Sidebar(props: SidebarProps) {
   }
 
   return (
-    <aside
-      className={clsx(
-        'border box-border border-grey-light text-grey-dark h-[100vh] relative flex flex-col font-[Roboto,Helvetica,Arial,sans-serif] sidebar-container w-[300px]',
-        !isSidebarToggled ? 'translate-x-[300px]' : '',
-      )}
-      style={{
-        boxShadow:
-          '0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%), 0px 2px 4px -1px rgb(0 0 0 / 20%)',
-      }}
-    >
-      <header className="flex bg-grey-lighter px-[20px] pr-0 items-center justify-between select-none">
-        <aside className="flex items-center justify-between">
-          <button
-            className={clsx(
-              'text-[11px] py-[3px] px-[5px] rounded-[3px] text-grey-medium mr-[2px] hover:opacity-70 transition-opacity',
-              selectedTab === 'blocks' &&
-                'bg-[#42D3FC] text-white font-bold  cursor-default hover:opacity-100',
-            )}
-            onClick={() => setSelectedTab('blocks')}
-          >
-            Blocks
-          </button>
-          <button
-            className={clsx(
-              'text-[11px] py-[3px] px-[5px] rounded-[3px] text-grey-medium hover:opacity-50 transition-opacity',
-              selectedTab === 'documentation' &&
-                'bg-[#42D3FC] text-white font-bold cursor-default hover:opacity-100',
-            )}
-            onClick={() => setSelectedTab('documentation')}
-          >
-            Documentation
-          </button>
-        </aside>
-
-        <FontAwesomeIcon
-          icon={faClose}
-          className="text-grey-light cursor-pointer hover:text-grey-medium transition-colors px-[20px] py-[15px]"
-          onClick={onToggleSidebar}
-        />
-      </header>
-
-      <main className="flex flex-col flex-grow overflow-y-scroll">
-        {/* ! we can improve threading through composition */}
-        {selectedTab === 'blocks' ? (
-          <SidebarSectionBlocks {...props} />
-        ) : (
-          <SidebarSectionDocumentation />
+    <main className="tailwind">
+      <aside
+        className={clsx(
+          'border box-border border-grey-light text-grey-dark h-[100vh] relative flex flex-col font-[Roboto,Helvetica,Arial,sans-serif] sidebar-container',
+          isSidebarToggled ? 'w-[300px]' : 'w-0',
         )}
-      </main>
-
-      <footer className="text-[12px] text-grey-medium mb-[20px] mx-[20px] flex justify-between">
-        <p>
-          Version{' '}
-          <a
-            className="font-bold underline"
-            target="_blank"
-            rel="noreferrer"
-            href={CONFIG.versionUrl}
-          >
-            {CONFIG.version}
-          </a>
-        </p>
-
-        <p>
-          Made w/ ❤️ by{' '}
-          <a
-            href="https://github.com/drewrodrigues"
-            className="font-bold underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Drew
-          </a>
-        </p>
-      </footer>
-
-      <button
-        className="absolute left-[-37px] bottom-[60px] border border-grey-light p-[5px] flex justify-center items-center rounded-[5px_0_0_5px]"
-        onClick={onToggleSidebar}
+        style={{
+          boxShadow:
+            '0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%), 0px 2px 4px -1px rgb(0 0 0 / 20%)',
+        }}
       >
-        <img
-          src={CONFIG.logoPath}
-          alt="Blocky Logo"
-          className="w-[25px] h-[25px]"
-        />
-      </button>
-    </aside>
+        <header className="flex bg-grey-lighter px-[20px] pr-0 items-center justify-between select-none">
+          <aside className="flex items-center justify-between">
+            <button
+              className={clsx(
+                'text-[11px] py-[3px] px-[5px] rounded-[3px] text-grey-medium mr-[2px] hover:opacity-70 transition-opacity',
+                selectedTab === 'blocks' &&
+                  'bg-[#42D3FC] text-white font-bold  cursor-default hover:opacity-100',
+              )}
+              onClick={() => setSelectedTab('blocks')}
+            >
+              Blocks
+            </button>
+            <button
+              className={clsx(
+                'text-[11px] py-[3px] px-[5px] rounded-[3px] text-grey-medium hover:opacity-50 transition-opacity',
+                selectedTab === 'documentation' &&
+                  'bg-[#42D3FC] text-white font-bold cursor-default hover:opacity-100',
+              )}
+              onClick={() => setSelectedTab('documentation')}
+            >
+              Documentation
+            </button>
+          </aside>
+
+          <FontAwesomeIcon
+            icon={faClose}
+            className="text-grey-light cursor-pointer hover:text-grey-medium transition-colors px-[20px] py-[15px]"
+            onClick={onToggleSidebar}
+          />
+        </header>
+
+        <main className="flex flex-col flex-grow overflow-y-scroll">
+          {/* ! we can improve threading through composition */}
+          {selectedTab === 'blocks' ? (
+            <SidebarSectionBlocks {...props} />
+          ) : (
+            <SidebarSectionDocumentation />
+          )}
+        </main>
+
+        <footer className="text-[12px] text-grey-medium mb-[20px] mx-[20px] flex justify-between">
+          <p>
+            Version{' '}
+            <a
+              className="font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+              href={CONFIG.versionUrl}
+            >
+              {CONFIG.version}
+            </a>
+          </p>
+
+          <p>
+            Made w/ ❤️ by{' '}
+            <a
+              href="https://github.com/drewrodrigues"
+              className="font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Drew
+            </a>
+          </p>
+        </footer>
+
+        <button
+          className="absolute left-[-37px] bottom-[60px] border border-grey-light p-[5px] flex justify-center items-center rounded-[5px_0_0_5px]"
+          onClick={onToggleSidebar}
+        >
+          <img
+            src={CONFIG.logoPath}
+            alt="Blocky Logo"
+            className="w-[25px] h-[25px]"
+          />
+        </button>
+      </aside>
+    </main>
   )
 }
