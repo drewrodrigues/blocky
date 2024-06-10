43c625ed688b49e6870d806ef1416f85ce983f9b
Drew Rodrigues <rodandrew95@gmail.com>
2024-06-10 11:03:41 -0700

    Fix calendar selection from UI change

--- plugin/README.md
+++ plugin/README.md
@@ -5,3 +5,7 @@
 # Updating
 
 When updating, bump the Git tag, `manifest.json` version and run `npm run package`. Then upload the zip to the [Chrome Web Store Developer Dashboard](https://chrome.google.com/webstore/devconsole/389ee976-6ace-4817-80ff-f860e391680a/cpcobiephkdkdgpkpfhcgbdokhcimcmi/edit).
+
+# Todo
+
+- [ ] Add error handling when UI selection/clicking does not work correctly.
--- plugin/src/utils/consts.ts
+++ plugin/src/utils/consts.ts
@@ -3,7 +3,7 @@ export enum CALENDAR_SELECTOR {
   MODAL = '.K0f0Xc',
   MODAL_TITLE_INPUT = '[aria-label="Add title"]',
   CALENDAR_OPTION_BUTTON = '[data-key="calendar"]',
-  CALENDAR_OPTION = 'div[role="listbox"][aria-label="Calendar"] > div[role="presentation"] > div',
+  CALENDAR_OPTION = '[aria-label="List of calendars"] [role="option"]',
   SAVE_BUTTON = '[jsname="x8hlje"]',
   SIDEBAR_CONTAINER = '.tEhMVd',
 }
